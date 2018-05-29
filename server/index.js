eba71455789aba951df9d8bc10fa2597f5dc3211
Mac Read <james.macgregor.read@gmail.com>
2018-05-29 13:49:05 -0600

    Step 2

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ require ('dotenv').config();
 const express = require('express')
     , bodyParser = require('body-parser')
     , session = require('express-session')
+
+const {SERVER_PORT, SESSION_SECRET} = process.env;
+// const mid = require('./middleware')
+const app = express();
+
+app.use(bodyParser.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening on port: ${SERVER_PORT}`)
+})
