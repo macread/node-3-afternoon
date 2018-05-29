9b0c250585504a0e9bab972cacf5663d40eae866
Mac Read <james.macgregor.read@gmail.com>
2018-05-29 15:53:34 -0600

    Step 9

--- server/controllers/cart_controller.js
+++ server/controllers/cart_controller.js
@@ -3,9 +3,9 @@ const swag = require('../models/swag');
 module.exports = {
     add: (req, res, next) => {
         let {cart} = req.session.user;
-        let idx = cart.findIndex( item => item.id === req.query.id);
+        let idx = cart.findIndex(item => item.id === req.query.id);
         if (idx < 0) {
-            let swagItem = swag.find( swagItem => swagItem.id === req.query.id);
+            let swagItem = swag.find(item => item.id == req.query.id);
             cart.push(swagItem)
             req.session.user.total += swagItem.price;
         }
@@ -14,16 +14,16 @@ module.exports = {
 
     delete: (req,res, next) =>{
         let {cart} = req.session.user;
-        let swagItem = swag.find( swagItem => swagItem.id === req.query.id);
+        let swagItem = swag.find( swagItem => swagItem.id == req.query.id);
 
-        let idx = cart.findIndex( item => item.id === req.query.id);
+        let idx = cart.findIndex( item => item.id == req.query.id);
         cart.splice(idx,1);
         req.session.user.total -= swagItem.price;
 
         res.status(200).send(req.session.user);
     },
 
-    checkout: (reg, res, next) => {
+    checkout: (req, res, next) => {
         req.session.user.cart=[];
         req.session.user.total=0;
         res.status(200).send(req.session.user);
--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const checkForSession = require('./middlewares/checkForSession');
 
 // Controllers
 const swag_controller = require('./controllers/swag_controller.js');
-const auth_controller = require('./controllers/auth_controller')
+const auth_controller = require('./controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
 
 const app = express();
 
@@ -24,12 +25,17 @@ app.use(session({
 
 app.use(checkForSession);
 
-app.get('/api/swag', swag_controller.read )
+app.get('/api/swag', swag_controller.read );
 
-app.post('/api/login', auth_controller.login)
-app.post('/api/register', auth_controller.register)
-app.post('/api/signout', auth_controller.signout)
-app.get('/api/user', auth_controller.getuser)
+app.post('/api/login', auth_controller.login);
+app.post('/api/register', auth_controller.register);
+app.post('/api/signout', auth_controller.signout);
+app.get('/api/user', auth_controller.getuser);
+
+app.post('/api/cart/', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete)
+
 
 app.listen(SERVER_PORT, () => {
     console.log(`Listening on port: ${SERVER_PORT}`)
