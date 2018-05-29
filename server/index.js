f7bff43a9c5dcdff3046490b4ca2e85e4eee0d52
Mac Read <james.macgregor.read@gmail.com>
2018-05-29 13:56:47 -0600

    Step 4

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
     , session = require('express-session')
 
 const {SERVER_PORT, SESSION_SECRET} = process.env;
-// const mid = require('./middleware')
+const checkForSession = require('./middlewares/checkForSession')
 const app = express();
 
 app.use(bodyParser.json())
@@ -13,6 +13,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
+
+app.use(checkForSession);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Listening on port: ${SERVER_PORT}`)
